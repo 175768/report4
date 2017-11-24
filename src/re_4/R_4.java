9c32a4ab798f71da1338f6510a0327da2e97b2e6
Hayato Tamaki <e175768@ie.u-ryukyu.ac.jp>
2017-11-24 11:17:33 +0900

    NullPointerException & null

--- src/re_4/R_4.java
+++ src/re_4/R_4.java
@@ -1,6 +1,14 @@
 public class R_4 {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+       try {
+           String str = null;
+           System.out.println(str.length());
+       }
+       catch (NullPointerException e) {
+           System.out.println(e);
+           System.out.println(e.getMessage());
+       }
+
     }
 }
+
